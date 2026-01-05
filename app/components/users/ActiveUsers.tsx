00d988779676a91137bb3ae89373bda110269ea7
shubhamsonni <9896shubham@gmail.com>
2026-01-05 15:26:38 +0530

    navbar created properly and now showing active users correctly

--- app/components/users/ActiveUsers.tsx
+++ app/components/users/ActiveUsers.tsx
@@ -1,6 +1,7 @@
 import { useOthers, useSelf } from "@liveblocks/react";
 import { Avatar } from "./Avatar";
 import styles from './index.module.css'
+import { generateRandomName } from "@/lib/utils";
 
 const ActiveUsers=() =>{
   const users = useOthers();
@@ -8,21 +9,20 @@ const ActiveUsers=() =>{
   const hasMoreUsers = users.length > 3;
 
   return (
-    <main className="flex h-screen w-full select-none place-content-center place-items-center">
+    <main className="flex items-center justify-center gap-1">
       <div className="flex pl-3">
+    {currentUser && (
+    <Avatar name="You" otherStyles="border-[3px] border-primary-green" />
+    )}
+
         {users.slice(0, 3).map(({ connectionId, info }) => {
           return (
-            <Avatar key={connectionId} src={info.avatar} name={info.name} />
+            <Avatar key={connectionId} name={generateRandomName()} otherStyles="-ml-3"/>
           );
         })}
 
         {hasMoreUsers && <div className={styles.more}>+{users.length - 3}</div>}
 
-        {currentUser && (
-          <div className="relative ml-8 first:ml-0">
-            <Avatar src={currentUser.info.avatar} name="You" />
-          </div>
-        )}
       </div>
     </main>
   );
